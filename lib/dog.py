b5e3f149b83a957f3ab379b0b7d06a5fb7ec74ee
Igor Rakush <irakush@aol.com>
2024-01-10 15:37:36 -0500

    pass the test

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+
 class Dog:
     # Class body goes here
+    def sit(self):
+        print("The dog is sitting.")
 
-    #Instance method definition
+    def bark(self):
+        print("Woof!")
+
+    # Instance method definition
     pass
